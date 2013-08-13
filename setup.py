ae3118792934c750fa3c8eb693468526558f7070
phil.roche <phil.roche@ticket-text.com>
2013-08-13 17:48:00 +0000

    Added manifest to include templates

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     license='MIT License',
-    latforms='any',
+    platforms='any',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
